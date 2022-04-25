d85c01fa1f02a4899918f3c05d90cd25e3ec8bce
Ianka Karuzia <iankakaruzia@gmail.com>
2022-04-24 22:51:04 -0300

    chore: add helmet and enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import helmet from 'helmet'
 import { AppModule } from './app.module'
 import { PrismaService } from './prisma/prisma.service'
 
@@ -12,12 +13,16 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors()
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
   const prismaService: PrismaService = app.get(PrismaService)
   prismaService.enableShutdownHooks(app)
 
   app.enableShutdownHooks()
+
+  app.use(helmet())
 
   const config = new DocumentBuilder()
     .setTitle('Invoices API')
