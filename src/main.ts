8d0b3bcc821e44ae2d5a3a587adcc7af9345936e
Ianka Karuzia <iankakaruzia@gmail.com>
2022-04-24 22:58:34 -0300

    chore: enable api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe, VersioningType } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
@@ -14,6 +14,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors()
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1'
+  })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
 
