66992fd2e527045cb40900a52a84ae375cae5cf1
Ianka Karuzia <iankakaruzia@gmail.com>
2022-04-28 23:12:14 -0300

    test: ensure theme and profile are updated correctly

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { User as UserModel } from '@prisma/client'
 import { PrismaService } from '../prisma/prisma.service'
 import { UploadService } from '../upload/upload.service'
 import { UsersService } from './users.service'
 
 describe('UsersService', () => {
   let service: UsersService
+  const prismaServiceMock = {
+    user: {
+      update: jest.fn()
+    }
+  }
+  const uploadServiceMock = {
+    uploadStream: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
-        { provide: UploadService, useValue: { uploadStream: jest.fn() } },
-        PrismaService
+        { provide: UploadService, useValue: uploadServiceMock },
+        { provide: PrismaService, useValue: prismaServiceMock }
       ]
     }).compile()
 
@@ -20,5 +29,35 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('uploadTheme', () => {
+    it('should call user update with correct theme', async () => {
+      await service.updateTheme({ theme: 'DARK' }, { id: 1 } as UserModel)
+
+      expect(prismaServiceMock.user.update).toHaveBeenCalledWith({
+        where: {
+          id: 1
+        },
+        data: {
+          preferredTheme: 'DARK'
+        }
+      })
+    })
+  })
+
+  describe('uploadProfile', () => {
+    it('should call user update with correct profile info', async () => {
+      await service.updateProfile({ name: 'any name' }, { id: 1 } as UserModel)
+
+      expect(prismaServiceMock.user.update).toHaveBeenCalledWith({
+        where: {
+          id: 1
+        },
+        data: {
+          name: 'any name'
+        }
+      })
+    })
   })
 })
